86d62cb5d629d7743629c17c18acbfdb37369625
Mateusz <mateusz.jama@gmail.com>
2020-11-04 20:08:18 +0100

    styling main component

---
+++ lottery/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <LotteryMainComponent></LotteryMainComponent>
+  </div>
+</template>
+
+<script>
+import LotteryMainComponent from "@/components/LotteryMainComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    LotteryMainComponent,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./assets/scss/main";
+</style>
---
+++ lottery/src/components/LotteryMainComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="lottery">
+    <div class="neon-container">
+      <div class="neon-text">
+        <h1>Lottery! Check and win!</h1>
+      </div>
+    </div>
+
+    <div class="lottery-informations">
+      <div class="name-lottery-container">Witaj Mateusz</div>
+      <div class="result-lottery-container">Poznaj wyniki</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+
+<style scoped lang="scss">
+.neon-text {
+  font-family: neon;
+}
+</style>

d69b04ea63984d27f8a8224177e9851fc6658435
Mateusz <mateusz.jama@gmail.com>
2020-11-04 21:21:33 +0100

    create modal to check adult

---
+++ lottery/src/components/IfAdultModalComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="modal-backdrop">
+    <div class="modal">
+      <header class="modal-header">
+        <slot name="header">
+          <button type="button" class="btn-close" @click="close">x</button>
+        </slot>
+      </header>
+      <div class="modal-body">
+        <slot name="body">
+          <div class="cad-msg">
+            Dana konfiguracja nie widnieje w tabeli CAD. Podaj numer wentylatora
+            pod którym wybrana konfiguracja będzie widniała w tabeli CAD:
+          </div>
+        </slot>
+      </div>
+      <footer class="modal-footer">
+        <slot name="footer">
+          <div class="navi-btns"></div>
+        </slot>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IfAdultModalComponent",
+  data() {
+    return {
+      value: "",
+      isCorrect: false,
+    };
+  },
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- lottery/src/components/LotteryMainComponent.vue
+++ lottery/src/components/LotteryMainComponent.vue
@@ -7,24 +7,50 @@
     </div>
 
     <div class="lottery-informations">
-      <div class="name-lottery-container">Witaj Mateusz</div>
+      <div class="name-lottery-container">
+        Witaj {{ user }}
+        <!-- <button @click="openModal()">modal</button> -->
+      </div>
       <div class="result-lottery-container">Poznaj wyniki</div>
     </div>
+    <IfAdultModalComponent
+      v-show="isModalVisible"
+      @close="closeModal"
+    ></IfAdultModalComponent>
   </div>
 </template>
 
 <script>
+import IfAdultModalComponent from "@/components/IfAdultModalComponent.vue";
+
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
+  name: "LotteryMainComponent",
+  components: { IfAdultModalComponent },
+  data() {
+    return {
+      user: "Mateusz",
+      isModalVisible: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.isModalVisible = true;
+    },
+
+    closeModal() {
+      this.isModalVisible = false;
+    },
+  },
+  beforeMount() {
+    this.openModal();
   },
 };
 </script>
 
 
 <style scoped lang="scss">
-.neon-text {
-  font-family: neon;
+button {
+  width: 100px;
+  height: 30px;
 }
 </style>
